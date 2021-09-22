ec90b2f48b7abce152f20589c9f129f7b6e1d42f
Marc Jaramillo <mnj.webdeveloper@gmail.com>
2021-09-22 16:15:49 -0700

    Separate attributes with commas

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
-  validates :name :address :price :rating :phone :image_url :url :user_id, presence: true
+  validates :name, :address, :price, :rating, :phone, :image_url, :url, :user_id, presence: true
 
   belongs_to :user
 end
