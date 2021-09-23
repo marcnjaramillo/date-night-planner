d307c5e8be1ce407f0054b5325b634c471448800
stewartm12 <stewartmorales129@gmail.com>
2021-09-23 00:14:52 -0400

    added devise custom functionality

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
-class WelcomeController < ApplicationController
-    def index
-    end
+class WelcomeController < ApplicationController 
+  def index 
+    render :index
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :user
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  authenticated :user do
-    root "welcome#index", as: "authenticated_root"
-  end
+  devise_scope :user do
+    authenticated :user do
+      root "welcome#index", as: "authenticated_root"
+    end
 
-  root "welcome#index"
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
+  
 
 end
